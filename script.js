23894022402387a23bb350c082749959eccff626
HarmenSc <harmens48@gmail.com>
2026-01-28 20:38:44 +0100

    Add code to create 16x16 div elements

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const container = document.createElement("div")
 container.id = "container"
 
 document.body.appendChild(container)
+
+const gridXY = 16
+
+for (let j = 0; j < gridXY; j++) {
+    const gridDivY = document.createElement("div")
+    gridDivY.className = 'gridBoxY'
+    container.appendChild(gridDivY)
+    for (let i = 0; i < gridXY; i++) {
+        const gridDivX = document.createElement("div");
+        gridDivX.className = 'gridBox';
+        gridDivX.textContent = `${i + 1}, ${j + 1}`;
+        gridDivY.appendChild(gridDivX)
+    }}
