ec97449d84505af75e8e3f7cf6499d1cfa31cf90
HarmenSc <harmens48@gmail.com>
2026-01-29 12:38:30 +0100

    Add functionality to create grid button, now creates grids with prompted dimensions

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const resetButton = document.createElement("button")
 
 resetButton.style.margin = '0 auto'
-resetButton.textContent = "Reset"
-
+resetButton.textContent = "Create Grid"
 
 const container = document.createElement("div")
 container.id = "container"
@@ -10,8 +9,15 @@ container.id = "container"
 document.body.appendChild(container)
 container.appendChild(resetButton)
 
+let gridDimension = 16
 
-const gridXY = 50
+resetButton.addEventListener("click", () => {
+    gridDimension = prompt("Enter grid dimension:")
+    console.log(gridDimension)
+    createGrid(gridDimension)
+})
+
+
 const gridSquareWidth = '50px'
 const fullGridSize = '720px'
 
@@ -19,35 +25,39 @@ container.style.height = fullGridSize
 container.style.width = fullGridSize
 container.style.margin = '0 auto'
 
-for (let j = 0; j < gridXY; j++) {
-    const gridDivY = document.createElement("div")
-    gridDivY.className = 'gridBoxY'
-    container.appendChild(gridDivY)
-    gridDivY.style.display = 'flex';
-    gridDivY.style.flexDirection = 'row';
-    gridDivY.style.justifyContent = 'center';
-    gridDivY.style.flex = '1 1 0';
+function createGrid(gridDimension) {
+    document.querySelectorAll('.gridBoxY').forEach(el => el.remove());
 
-    for (let i = 0; i < gridXY; i++) {
-        const gridDivX = document.createElement("div");
-        gridDivX.className = 'gridBox';
-        gridDivY.appendChild(gridDivX);
-        gridDivX.style.flex = '1 1 0';
-        gridDivX.style.aspectRatio = '1 / 1';
-        gridDivX.style.borderStyle = 'solid';
-        gridDivX.style.borderWidth = '1px';
-        gridDivX.style.textAlign = 'center';
+    for (let j = 0; j < gridDimension; j++) {
+        const gridDivY = document.createElement("div")
+        gridDivY.className = 'gridBoxY'
+        container.appendChild(gridDivY)
+        gridDivY.style.display = 'flex';
+        gridDivY.style.flexDirection = 'row';
+        gridDivY.style.justifyContent = 'center';
+        gridDivY.style.flex = '1 1 0';
 
-    }}
+        for (let i = 0; i < gridDimension; i++) {
+            const gridDivX = document.createElement("div");
+            gridDivX.className = 'gridBox';
+            gridDivY.appendChild(gridDivX);
+            gridDivX.style.flex = '1 1 0';
+            gridDivX.style.aspectRatio = '1 / 1';
+            gridDivX.style.borderStyle = 'solid';
+            gridDivX.style.borderWidth = '1px';
+            gridDivX.style.textAlign = 'center';
 
-container.style.display = 'flex';
-container.style.flexDirection = 'column';
-container.style.justifyContent = 'stretch'
+        }}
 
-const gridBoxes = document.querySelectorAll('.gridBox');
+    container.style.display = 'flex';
+    container.style.flexDirection = 'column';
+    container.style.justifyContent = 'stretch'
 
-gridBoxes.forEach(box => {
-    box.addEventListener("mouseenter", () => {
-        box.style.backgroundColor = 'black';
+    const gridBoxes = document.querySelectorAll('.gridBox');
+
+    gridBoxes.forEach(box => {
+        box.addEventListener("mouseenter", () => {
+            box.style.backgroundColor = 'black';
+        });
     });
-});
+}
