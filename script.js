c4ba1858bf73702e059ecaa6926a0610bd6371e5
HarmenSc <harmens48@gmail.com>
2026-01-29 12:05:34 +0100

    Add automatic size adjustment based on the amount of squares in the grid

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ document.body.appendChild(container)
 
 const gridXY = 16
 const gridSquareWidth = '50px'
+const fullGridSize = '960px'
+
+container.style.height = fullGridSize
+container.style.width = fullGridSize
+container.style.margin = '0 auto'
 
 for (let j = 0; j < gridXY; j++) {
     const gridDivY = document.createElement("div")
@@ -12,23 +17,24 @@ for (let j = 0; j < gridXY; j++) {
     container.appendChild(gridDivY)
     gridDivY.style.display = 'flex';
     gridDivY.style.flexDirection = 'row';
-    gridDivY.style.justifyContent = 'center'
+    gridDivY.style.justifyContent = 'center';
+    gridDivY.style.flex = '1 1 0';
 
     for (let i = 0; i < gridXY; i++) {
         const gridDivX = document.createElement("div");
         gridDivX.className = 'gridBox';
-        gridDivY.appendChild(gridDivX)
-        gridDivX.style.height = gridSquareWidth
-        gridDivX.style.width = gridSquareWidth
-        gridDivX.style.borderStyle = 'solid'
-        gridDivX.style.borderWidth = '1px'
-        gridDivX.style.textAlign = 'center'
+        gridDivY.appendChild(gridDivX);
+        gridDivX.style.flex = '1 1 0';
+        gridDivX.style.aspectRatio = '1 / 1';
+        gridDivX.style.borderStyle = 'solid';
+        gridDivX.style.borderWidth = '1px';
+        gridDivX.style.textAlign = 'center';
 
     }}
 
 container.style.display = 'flex';
 container.style.flexDirection = 'column';
-container.style.justifyContent = 'center'
+container.style.justifyContent = 'stretch'
 
 const gridBoxes = document.querySelectorAll('.gridBox');
 
